34afb2b9ae0ab284075ac8c5a68393c1f5731cb1
Zulfian <zulfian.nutech@gmail.com>
2021-02-25 13:20:09 +0700

    testing

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, NativeModules} from 'react-native';
+import {API_URL_DEV, API_URL_STA, API_URL_PROD} from '@env';
 import CodePush from 'react-native-code-push';
 import Home from './src/screens/Home';
 
 let codePushOptions = {checkFrequency: codePush.CheckFrequency.ON_APP_RESUME};
 
-let App = () => <Home />;
+function selectEnv(envs) {
+  return envs[NativeModules.RNConfig.env] || envs.default;
+}
+
+const baseURL = selectEnv({
+  dev: API_URL_DEV,
+  staging: API_URL_STA,
+  prod: API_URL_PROD,
+});
+
+let App = () => {
+  const env = NativeModules.RNConfig.env;
+  return <Home env={env} baseURL={baseURL} />;
+};
 
 App = CodePush(codePushOptions)(App);
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import {NativeModules, StyleSheet, Text, View} from 'react-native';
 
-function selectEnv(envs) {
-  return envs[NativeModules.RNConfig.env] || envs.default;
-}
-
-const baseURL = selectEnv({
-  dev: 'http://localhost:3000',
-  staging: 'http://staging.com/api',
-  prod: 'http://prod.com/api',
-});
-
-const Home = () => {
-  const env = NativeModules.RNConfig.env;
-
+const Home = ({env, baseURL}) => {
   return (
     <View>
       <Text>Home Screen</Text>
