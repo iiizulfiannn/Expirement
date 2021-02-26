638d2668d1cce1b4660e6f3628912812b1c25d81
Zulfian <zulfian.nutech@gmail.com>
2021-02-26 10:12:43 +0700

    update

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {StyleSheet, Text, View, NativeModules} from 'react-native';
-import {API_URL_DEV, API_URL_STA, API_URL_PROD} from '@env';
 import CodePush from 'react-native-code-push';
 import Home from './src/screens/Home';
 
-let codePushOptions = {checkFrequency: codePush.CheckFrequency.ON_APP_RESUME};
+let codePushOptions = {checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME};
 
 function selectEnv(envs) {
   return envs[NativeModules.RNConfig.env] || envs.default;
 }
 
 const baseURL = selectEnv({
-  dev: API_URL_DEV,
-  staging: API_URL_STA,
-  prod: API_URL_PROD,
+  dev: 'http://localhost:3000',
+  development: 'http://localhost:3000',
+  staging: 'http://staging.com/api',
+  production: 'http://prod.com/api',
 });
 
 const env = NativeModules.RNConfig.env;
